f900d3a0756c0d4455b63f35cc683f09962d52ee
Irina Filippova <irina.filippova.mail@yandex.ru>
2022-04-18 21:40:32 +0300

    Add HorizontalLine

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using Snake;
+using Snake_oop;
 
 class Program
 {
@@ -13,25 +14,8 @@ class Program
         Point p2 = new Point(5, 7, '#');
         p2.Draw();
 
-        List<int> numlist = new List<int>();
-        numlist.Add(0);
-        numlist.Add(1);
-        numlist.Add(2);
-
-        int x = numlist[0];
-        int y = numlist[1];
-        int z = numlist[2];
-
-        foreach (int i in numlist)
-        {
-            Console.WriteLine(i);
-        }
-
-        numlist.RemoveAt(0);
-
-        List<Point> plist = new List<Point>();
-        plist.Add(p1);
-        plist.Add(p2);
+        HorizontalLine line = new HorizontalLine();
+        line.Drow();
 
         Console.ReadLine();
     }

acabbed4b44d685fe488be86bb7f6c31757f78a4
Irina Filippova <irina.filippova.mail@yandex.ru>
2022-04-26 21:06:00 +0300

    Adding method HandleKey

--- Program.cs
+++ Program.cs
@@ -18,27 +18,7 @@ namespace Snake_oop;
 
         
 
-        Point p = new Point(4, 3, '+');
-        Snake snake = new Snake(p, 4, Direction.RIGHT);
-        snake.Drow();
-        snake.Move();
-        Thread.Sleep(300);
-        snake.Move();
-        Thread.Sleep(300);
-        snake.Move();
-        Thread.Sleep(300);
-        snake.Move();
-        Thread.Sleep(300);
-        snake.Move();
-        Thread.Sleep(300);
-        snake.Move();
-        Thread.Sleep(300);
-        snake.Move();
-        Thread.Sleep(300);
-        snake.Move();
-        Thread.Sleep(300);
-        snake.Move();
-        Thread.Sleep(300);
+        
 
 
         HorizontalLine upLine = new HorizontalLine(0, 78, 0, '*');
@@ -49,8 +29,24 @@ namespace Snake_oop;
         leftLine.Drow();
         VerticalLine rightLine = new VerticalLine(0, 24, 78, '*');
         rightLine.Drow();
+
+        Point p = new Point(4, 3, '+');
+        Snake snake = new Snake(p, 4, Direction.RIGHT);
+        snake.Drow();
+        snake.Move();
         
-       
+        while(true)
+        {
+            if(Console.KeyAvailable)
+            {
+                ConsoleKeyInfo key = Console.ReadKey();
+                snake.HandleKey(key.Key);
+            }
+            Thread.Sleep(100);
+            snake.Move();
+        }
+
+        Console.Read();
 
     }
 }
